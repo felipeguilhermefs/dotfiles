6ec27c3aa388568ed1449378393db7df207ed6b2
Felipe Guilherme <felipeguilhermefs@gmail.com>
2018-08-26 12:33:46 -0300

    Add pc screen laoyouts

---
+++ .screenlayout/double-home.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1366x768 --pos 1920x312 --rotate normal --output VIRTUAL1 --off
+feh --bg-scale ~/Pictures/manifesto.png
