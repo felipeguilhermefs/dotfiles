17119f86ed17af87d4486823256acb61db9719c2
Felipe Flores <felipeguilhermefs@gmail.com>
2024-03-22 00:45:58 +0100

    Add neovim configs

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,9 @@
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>o', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>O', builtin.find_files, {})
+
+vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+
+vim.keymap.set('n', '<leader>c', builtin.colorscheme, {})
---
+++ nvim/lua/ffdev/packer.lua
@@ -0,0 +1,23 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Telescope
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Tokyo Night
+  use {
+	  'folke/tokyonight.nvim',
+	  as = 'tokyonight',
+	  config = function()
+		  vim.cmd('colorscheme tokyonight')
+	  end
+  }
+end)
