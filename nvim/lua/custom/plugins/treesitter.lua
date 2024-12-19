6edffc371a391ca5372909d385e8015d565b7e32
Felipe Flores <felipeguilhermefs@gmail.com>
2024-12-19 23:00:32 +0100

    Move LSP and TreeSitter to own file

---
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+-- Highlight, edit, and navigate code
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  opts = {
+    ensure_installed = {
+      'bash',
+      'c',
+      'diff',
+      'html',
+      'lua',
+      'luap',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'sql',
+      'vim',
+      'vimdoc',
+      'yaml',
+    },
+    auto_install = false,
+    highlight = {
+      enable = true,
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    -- Prefer git instead of curl in order to improve connectivity in some environments
+    require('nvim-treesitter.install').prefer_git = true
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup(opts)
+  end,
+}
