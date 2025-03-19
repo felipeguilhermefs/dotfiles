2c42a9ead2ef6d6caedb0e1f1bfbb3a9e329b992
Felipe Flores <felipeguilhermefs@gmail.com>
2025-03-19 18:46:47 +0100

    Neovim: Substitute Telescope with Snacks picker

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -33,37 +33,6 @@ return {
           vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        local telescope = require 'telescope.builtin'
-
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-t>.
-        map('gd', telescope.lsp_definitions, '[g]oto [d]efinition')
-
-        -- Find references for the word under your cursor.
-        map('gr', telescope.lsp_references, '[g]oto [r]eferences')
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', telescope.lsp_implementations, '[g]oto [I]mplementation')
-
-        -- This is Goto Declaration.
-        -- For example, in C this would take you to the header.
-        map('gD', vim.lsp.buf.declaration, '[g]oto [D]eclaration')
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        map('<leader>D', telescope.lsp_type_definitions, 'Type [D]efinition')
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        map('<leader>ds', telescope.lsp_document_symbols, '[d]ocument [s]ymbols')
-
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        map('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[w]orkspace [s]ymbols')
-
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
         map('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
@@ -140,7 +109,7 @@ return {
 
     require('mason').setup()
 
-    local formatters = { 'stylua' }
+    local formatters = { 'gofumpt', 'stylua' }
 
     local servers_and_formatters = vim.tbl_keys(servers)
     vim.list_extend(servers_and_formatters, formatters)
--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -9,24 +9,44 @@ return { -- QoL plugins
       size = 200 * 1024, -- 200KB
     },
     indent = { enabled = true },
+    picker = { enabled = true },
     profiler = { enabled = false },
     quickfile = { enabled = true },
     scope = { enabled = true },
   },
   keys = {
-    {
-      '<leader>fe',
-      function()
-        Snacks.dim.enable()
-      end,
-      desc = '[F]ocus [E]nable',
-    },
-    {
-      '<leader>fd',
-      function()
-        Snacks.dim.disable()
-      end,
-      desc = '[F]ocus [D]isable',
-    },
+    -- Focus
+    { '<leader>fe', function() Snacks.dim.enable() end, desc = '> [F]ocus [E]nable' },
+    { '<leader>fd', function() Snacks.dim.disable() end, desc = '> [F]ocus [D]isable' },
+    -- Files
+    { '<leader>sf', function() Snacks.picker.files() end, desc = '> [S]earch [F]iles' },
+    { '<leader>sb', function() Snacks.picker.buffers() end, desc = '> [S]earch in [B]uffers' },
+    { '<leader>sp', function() Snacks.picker.projects() end, desc = '> [S]earch in [P]roject' },
+    { '<leader>sr', function() Snacks.picker.recent() end, desc = '> [S]earch [R]ecent Files' },
+    { '<leader>sc', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = '> [S]earch [Config] Files' },
+    -- Grep
+    { '<leader>sG', function() Snacks.picker.grep() end, desc = '> [S]earch by [G]rep' },
+    { '<leader>sg', function() Snacks.picker.grep_buffers() end, desc = '> [S]earch by [g]rep on Buffers' },
+    { '<leader>/', function() Snacks.picker.lines() end, desc = '> [/] Fuzzy find in current buffer' },
+    -- Search
+    { '<leader>sh', function() Snacks.picker.help() end, desc = '> [S]earch [H]elp' },
+    { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '> [S]earch [K]eymaps' },
+    { '<leader>sq', function() Snacks.picker.qflist() end, desc = '> [S]earch [Q]uickfix List' },
+    { '<leader>sm', function() Snacks.picker.marks() end, desc = '> [S]earch [M]arks' },
+    -- Git
+    { '<leader>gb', function() Snacks.picker.git_branches() end, desc = "> [G]it [B]ranches" },
+    { '<leader>gl', function() Snacks.picker.git_log() end, desc = "> [G]it [L]og" },
+    { '<leader>gs', function() Snacks.picker.git_status() end, desc = "> [G]it [s]tatus" },
+    { '<leader>gS', function() Snacks.picker.git_stash() end, desc = "> [G]it [S]tash" },
+    { '<leader>gd', function() Snacks.picker.git_diff() end, desc = "> [G]it [D]iff (Hunks)" },
+    { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = "> [G]it Log [F]ile" },
+    -- LSP
+    { 'gd', function() Snacks.picker.lsp_definitions() end, desc = '[g]oto [d]efinition' },
+    { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = '[g]oto [r]eferences' },
+    { 'gD', function() Snacks.picker.lsp_declarations() end, desc = '[g]oto [D]eclaration' },
+    { 'gi', function() Snacks.picker.lsp_implementations() end, desc = '[g]oto [i]mplementation' },
+    { '<leader>D', function() Snacks.picker.lsp_type_definitions() end, desc = 'Type [D]efinition' },
+    { '<leader>ds', function() Snacks.picker.lsp_symbols() end, desc = '[d]ocument [s]ymbols' },
+    { '<leader>ws', function() Snacks.picker.lsp_workspace_symbols() end, desc = '[w]orkspace [s]ymbols' },
   },
 }
